27275cbb49aa011fc43f13bed8417b643891b654
Victoria Sawchuk <vtsawchuk@gmail.com>
2017-08-18 10:46:40 -0700

    Complete Wave 1: all tests pass

--- Rakefile
+++ Rakefile
@@ -4,7 +4,7 @@ Rake::TestTask.new do |t|
   t.libs = ["lib"]
   t.warning = true
   t.test_files = FileList['specs/*_spec.rb']
+  t.warning = false
 end
 
 task default: :test
-
--- lib/card.rb
+++ lib/card.rb
@@ -5,9 +5,16 @@ class Card
   attr_reader :value, :suit
 
   def initialize(value, suit)
+    if (1..13).to_a.include?(value) == false
+      raise ArgumentError.new("The value must be an integer between 1 and 13.")
+    end
+    if [:clubs, :diamonds, :hearts, :spades].include?(suit) == false
+      raise ArgumentError.new("The suit must be clubs, diamonds, hearts, or spades.")
+    end
+
     @value = value
     @suit = suit
-    
+
 
   end
 
